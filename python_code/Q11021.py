0bd1c4fac28cdc554938aa9516d5352d0ccfafe2
unknown <kimsw6246@naver.com>
2020-01-18 22:19:22 +0900

    Q11022.py

--- python_code/Q11021.py
+++ python_code/Q11021.py
@@ -1,4 +1,4 @@
 N=int(input())
-for i in range(1, N+1):
-    a, b= map(int, input().split())
-    print('Case #%d: %d' %(i, a+b))
+for i in range(1,N+1):
+    a, b=map(int, input().split())
+    print('Case #%d: %d'%(i,a+b))
---
+++ python_code/Q11022.py
@@ -0,0 +1,5 @@
+N=int(input())
+for i in range(N):
+    a, b=map(int, input(),split())
+    n=a+b
+    print('Case #%d: %d + %d = %d'%(i+1, a, b, n)):
